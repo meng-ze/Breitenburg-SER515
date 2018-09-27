ac7594cb5a722b88a6c171efc3afaec5d517e911
aneeshdalvi <adalvi1@asu.edu>
2018-09-26 18:32:23 -0700

    US7 - Task#24 - Login Validation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,  redirect, url_for,  request
+from flask import Flask, render_template, redirect, url_for, request, flash
 #from data import Articles
 from flaskext.mysql import MySQL
 from wtforms import Form, StringField, PasswordField, validators
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # Config MySQL
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root123'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'web_forum'
 app.secret_key = 'super secret key'
 mysql.init_app(app)
@@ -28,6 +28,13 @@ class RegisterForm(Form):
     email = StringField('Email')
     password = PasswordField('Password')
 
+# Login Form Class
+
+
+class LoginForm(Form):
+    email = StringField('Email')
+    password = PasswordField('Password')
+
 
 # User Register
 @app.route('/register', methods=['GET', 'POST'])
@@ -38,14 +45,40 @@ def register():
         email = form.email.data
         password = form.password.data
 
-
         conn = mysql.connect()
         cur = conn.cursor()
-        cur.execute("INSERT INTO user(username, emailid, password, phone, dob, gender) VALUES(%s, %s, %s, %s, %s, %s)", (name, email, password, "","",'M'))
+        cur.execute("INSERT INTO user(username, emailid, password, phone, dob, gender) VALUES(%s, %s, %s, %s, %s, %s)", (name, email, password, "", "", 'M'))
         conn.commit()
         cur.close()
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
+
+# User login
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        password = form.password.data
+        flag = 0
+
+        conn = mysql.connect()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM user WHERE emailid = %s and password = %s", (email, password))
+
+        for (emailid) in cur:
+            print("{}".format(emailid))
+            flag = 1
+
+        cur.close()
+        if flag == 0:
+            flash('Incorrect username/password.')
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
