aef846b65bf71ab662ff2f774037792a2fc12923
Josh <jkdrumm1@gmail.com>
2018-10-28 22:08:17 -0700

    User Story: 6 Fixed a bug where the website would crash upon creating a new post Fixed a database error where the PK of comments was also a FK of user_id

--- SQL_Script/web_forum.sql
+++ SQL_Script/web_forum.sql
@@ -153,7 +153,7 @@ ALTER TABLE `user`
 -- Constraints for table `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`comment_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`) ON UPDATE CASCADE;
 
 --
