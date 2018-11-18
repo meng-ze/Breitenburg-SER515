ca8b676e002ee8c11f008587c32c779a75ea68e1
Josh <jkdrumm1@gmail.com>
2018-11-17 19:59:01 -0700

    US: 107, SP: 121 The Rule Modal appears when clicking on "Rules"

--- app.py
+++ app.py
@@ -403,4 +403,4 @@ def list_admin():
     return render_template('list_admin.html', admins_list=admins_list)  # <- Here you jump away from whatever result you create
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='127.0.0.1')
